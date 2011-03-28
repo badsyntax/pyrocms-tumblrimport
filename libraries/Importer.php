9fb474e072896a1b0a224b9c17934427f03afeb0
Richard <willis.rh@gmail.com>
2011-03-28 20:29:08 +0000

    removed blog_url from function params as its a public member

--- libraries/Importer.php
+++ libraries/Importer.php
@@ -18,10 +18,10 @@ abstract class Importer
 	abstract public function import_posts();
 	abstract public function import_pages();
 	abstract public function load_feed($feed_url);
-	abstract public function save_pages($pages, $status, $blog_url);
+	abstract public function save_pages($pages, $status);
 	abstract public function save_page($input);
-	abstract public function save_posts($posts, $status, $blog_url);
+	abstract public function save_posts($posts, $status);
 	abstract public function save_post($data);
 	abstract public function save_post_tags($tags);
-	abstract public function save_redirects($id, $data, $blog_url);
+	abstract public function save_redirects($id, $data);
 }
